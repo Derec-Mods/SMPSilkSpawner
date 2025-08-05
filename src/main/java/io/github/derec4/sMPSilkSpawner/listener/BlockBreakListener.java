e556e8c70a04582f1666bcf5867f124fa630aab9
Derek Chen <derex1987@gmail.com>
2025-08-05 17:38:26 -0500

    feat: mob spawner place sound

--- src/main/java/io/github/derec4/sMPSilkSpawner/listener/BlockBreakListener.java
+++ src/main/java/io/github/derec4/sMPSilkSpawner/listener/BlockBreakListener.java
@@ -1,8 +1,12 @@
 package io.github.derec4.sMPSilkSpawner.listener;
 
 import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
 
 public class BlockBreakListener implements Listener {
-    // Event handling logic will go here
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        // TODO: Add logic here
+    }
 }
-
--- src/main/java/io/github/derec4/sMPSilkSpawner/listener/BlockPlaceListener.java
+++ src/main/java/io/github/derec4/sMPSilkSpawner/listener/BlockPlaceListener.java
@@ -1,8 +1,24 @@
 package io.github.derec4.sMPSilkSpawner.listener;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
 
 public class BlockPlaceListener implements Listener {
     // Event handling logic will go here
-}
 
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (event.getBlock().getType() != Material.SPAWNER) {
+            return;
+        }
+
+        World world = event.getBlock().getWorld();
+        Location location = event.getBlock().getLocation();
+        world.playSound(location, Sound.BLOCK_SOUL_SAND_PLACE, 1.0f, 1.0f);
+    }
+}
